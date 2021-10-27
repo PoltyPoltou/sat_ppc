68306bda612d9230a82a9e08e2a1852f30a17ca3
NP-desktop <polt.gun@gmail.com>
2021-10-27 23:19:32 +0200

    first week model

--- solver_python/model.py
+++ solver_python/model.py
@@ -13,13 +13,26 @@ class Sgp:
         self.n = self.groups * self.size
         self.schedule = []
 
-    def get_basic_model(self):
+    def init_schedule(self):
         self.schedule = np.empty((self.weeks, self.groups), Set_var)
-        model = Model()
         for w in range(len(self.schedule)):
             for g in range(len(self.schedule[w])):
                 self.schedule[w, g] = Set_var(
                     [], list(range(self.n)), (self.size, self.size), "s[{},{}]".format(w, g))
+
+    def init_schedule_first_week(self):
+        self.schedule = np.empty((self.weeks, self.groups), Set_var)
+        for w in range(len(self.schedule)):
+            for g in range(len(self.schedule[w])):
+                if w == 0:
+                    self.schedule[w, g] = Set_var(
+                        range(g*self.size, (g+1)*self.size), range(g*self.size, (g+1)*self.size), (self.size, self.size))
+                else:
+                    self.schedule[w, g] = Set_var(
+                        [], list(range(self.n)), (self.size, self.size), "s[{},{}]".format(w, g))
+
+    def model_sgp(self):
+        model = Model()
         for w in range(self.weeks):
             for g1 in range(self.groups):
                 for g2 in range(g1):
@@ -35,8 +48,19 @@ class Sgp:
                             self.schedule[w1, g1], self.schedule[w2, g2], H))
         return model
 
+    def get_basic_model(self):
+        self.init_schedule()
+        return self.model_sgp()
+
+    def first_week_model(self):
+        self.init_schedule_first_week()
+        return self.model_sgp()
+
     def print_sol(self):
         for w in range(len(self.schedule)):
+            print("", end="| ")
             for g in range(len(self.schedule[w])):
-                print(self.schedule[w, g].lb, end=" | ")
+                for golfer in self.schedule[w, g].lb:
+                    print("{:<2}".format(golfer), end=" ")
+                print("", end="| ")
             print("")
