a25aa832e5362e047f73c354d899aea645946dbf
NP-desktop <polt.gun@gmail.com>
2021-10-27 23:19:43 +0200

    main pog

--- solver_python/main.py
+++ solver_python/main.py
@@ -1,10 +1,10 @@
 import model
 import solver
 import testing
-testing.test_all()
+# testing.test_all()
 
-a = model.Sgp(2, 2, 3)
-mdl = a.get_basic_model()
+a = model.Sgp(4, 3, 3)
+mdl = a.first_week_model()
 
 solver.solve(mdl, sgp=a)
 
