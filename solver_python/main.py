e00988fa9a62355ed30c03d5fbb54801533ea497
NP-desktop <polt.gun@gmail.com>
2021-10-29 00:34:09 +0200

    stuff on main

--- solver_python/main.py
+++ solver_python/main.py
@@ -1,19 +1,17 @@
 from ete3.treeview.faces import TextFace
-from ete3.treeview.main import TreeStyle
 import model
 import solver
 import testing
 from ete3 import Tree
 testing.test_all()
 
-a = model.Sgp(3, 3, 4)
+a = model.Sgp(4, 3, 3)
 mdl = a.first_week_model()
 t = Tree()
 solver.solve(mdl, sgp=a, tree=t)
 
-ts = TreeStyle()
 for node in t.traverse():
     if not node.is_leaf():
         node.add_face(TextFace(node.name), 0)
-t.render("test.png", tree_style=ts)
+t.render("test.png")
 a.print_sol()
