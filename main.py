7f3d756d2b2ca1a5b8060a810929bdc4df538353
NP-desktop <polt.gun@gmail.com>
2021-11-08 21:48:40 +0100

    main modified to be generic sat solvers in solver_set_sat

--- main.py
+++ main.py
@@ -1,80 +1,23 @@
 import multiprocessing
+from time import sleep
+from solver_python import testing
 import sys
-from time import sleep, time
-from set_sat import Set_Sat
-from set_sat_advanced import Set_Sat_Adv
-
-
-def sgp_set_to_sat(groups,
-                   size,
-                   weeks,
-                   out=sys.stdout):
-
-    model_start = time()
-    n_golfers = size * groups
-    sgp = Set_Sat()
-    schedule = [[0]*groups for i in range(weeks)]
-
-    for w in range(weeks):
-        for g in range(groups):
-            if w == 0:
-                schedule[w][g] = sgp.add_set_var(
-                    range(g*size, (g+1)*size), range(g*size, (g+1)*size))
-            else:
-                schedule[w][g] = sgp.add_set_var(
-                    [i for i in range(size) if i % groups == g], range(n_golfers))
-
-    for w in range(weeks):
-        for g in range(groups):
-            sgp.cardinal(schedule[w][g], size)
-
-    for w in range(weeks):
-        for g1 in range(groups):
-            for g2 in range(g1):
-                sgp.intersection_empty(schedule[w][g1], schedule[w][g2])
-
-    for w1 in range(weeks):
-        for w2 in range(w1):
-            for g1 in range(groups):
-                for g2 in range(groups):
-                    k = sgp.add_set_var([], range(n_golfers))
-                    sgp.intersection(
-                        schedule[w1][g1], schedule[w2][g2], k, True, True)
-                    sgp.cardinal_ub(k, 1)
-    for w in range(weeks):
-        sgp.order_by_min(schedule[w])
-    sgp.order_by_max([schedule[w][0] for w in range(weeks)])
-    model_end = time()
-
-    mdl = sgp.solve()
-    solve_end = time()
-    print("\n{}-{}-{}\nvariables : {} clauses : {}".format(groups, size,
-          weeks, sgp.idx_next_var-1, len(sgp.cnf.clauses)), file=out)
-    print("model : {:.2f}s solver : {:.2f}s".format(
-        model_end - model_start, solve_end - model_end), file=out)
-
-    sol = sgp.model_to_set_solution(mdl)
-    if sol != []:
-        for i in range(groups*weeks):
-            if i % groups == 0:
-                print("\n|", end="", file=out)
-            for elmt in sol[i]:
-                print("{:<2}".format(elmt), end=" ", file=out)
-            print("| ", end="", file=out)
-        print("", file=out)
-    else:
-        print("UNSAT", file=out)
-    return sol
+import os
+testing.test_all()
 
 
 def benchmark(name, g, s, weeks):
-    with open("./set_sat/{}_{}-{}.txt".format(name, g, s), "x") as f:
+    # runs all sgp problems and copy timings and infos in a file
+    if not os.path.exists("./ppc/" + name):
+        os.mkdir("./ppc/"+name)
+    with open("./ppc/{}/{}-{}.txt".format(name, g, s), "x") as f:
         for w in weeks:
-            sgp_set_to_sat(g, s, w, f)
+            solve(g, s, w, f, name)
             f.flush()
 
 
 def bench_core(name, g, s, weeks):
+    # start a thread on another CPU core for the given weeks
     th = multiprocessing.Process(
         target=lambda: benchmark(name, g, s, weeks))
     th.start()
@@ -82,31 +25,23 @@ def bench_core(name, g, s, weeks):
 
 
 if __name__ == "__main__":
-    if sys.argv[-1] == "test":
-
-        b = Set_Sat()
-        b.add_set_var([2], [1, 2, 3])
-        latch = b.latch_set(0)
-        b.cnf.append([-1])
-        mdl = b.solve()
-        print(b.model_to_set_solution(mdl))
-        for key in latch:
-            print(key, mdl[latch[key]-1])
-
-        c = Set_Sat_Adv()
-        c.add_set_var([], [1, 2, 3])
-        latch = c.latch_set(0)
-        c.cnf.append([-1])
-        c.cnf.append([-2])
-        mdl = c.solve()
-        print(c.model_to_set_solution(mdl))
-        for key in latch:
-            print(key, mdl[latch[key]-1])
-
-    elif len(sys.argv) == 4:
-        sgp_set_to_sat(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
-    elif len(sys.argv) == 3 and sys.argv[1] == "bench":
+    from solver_python.solver_ppc import solve_ppc
+    from solver_set_sat import solve_sgp_adv, solve_sgp_basic
+    solve = solve_sgp_basic
+    if not os.path.isdir("./ppc"):
+        os.mkdir("./ppc")
+    if len(sys.argv) == 4:
+        # usage : python main.py 5 3 3
+        # will add the pdf tree in ./ppc/pog/5-3-3.pdf
+        if not os.path.exists("./ppc/pog"):
+            os.mkdir("./ppc/pog")
+        solve(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), name="pog")
+    if len(sys.argv) == 3 and sys.argv[1] == "bench":
+        # usage : python main.py bench foo3
+        # will run 5-3 5-4 and 8-4 tests for 1h in *parrallel*
+        # all results in ./ppc/foo3/
         name = sys.argv[2]
+        os.mkdir("./ppc/{}".format(name))
         th = bench_core(name, 5, 3, range(1, 12))
         th1 = bench_core(name, 5, 4, range(1, 10))
         th2 = bench_core(name, 8, 4, range(1, 10))
---
+++ solver_set_sat.py
@@ -0,0 +1,75 @@
+import multiprocessing
+import sys
+from time import sleep, time
+from set_sat import Set_Sat
+from set_sat_advanced import Set_Sat_Adv
+
+
+def sgp_set_to_sat(groups,
+                   size,
+                   weeks,
+                   out=sys.stdout,
+                   sgp=Set_Sat()):
+
+    model_start = time()
+    n_golfers = size * groups
+    schedule = [[0]*groups for i in range(weeks)]
+
+    for w in range(weeks):
+        for g in range(groups):
+            if w == 0:
+                schedule[w][g] = sgp.add_set_var(
+                    range(g*size, (g+1)*size), range(g*size, (g+1)*size))
+            else:
+                schedule[w][g] = sgp.add_set_var(
+                    [i for i in range(size) if i % groups == g], range(n_golfers))
+
+    for w in range(weeks):
+        for g in range(groups):
+            sgp.cardinal(schedule[w][g], size)
+
+    for w in range(weeks):
+        for g1 in range(groups):
+            for g2 in range(g1):
+                sgp.intersection_empty(schedule[w][g1], schedule[w][g2])
+
+    for w1 in range(weeks):
+        for w2 in range(w1):
+            for g1 in range(groups):
+                for g2 in range(groups):
+                    k = sgp.add_set_var([], range(n_golfers))
+                    sgp.intersection(
+                        schedule[w1][g1], schedule[w2][g2], k, True, True)
+                    sgp.cardinal_ub(k, 1)
+    for w in range(weeks):
+        sgp.order_by_min(schedule[w])
+    sgp.order_by_max([schedule[w][0] for w in range(weeks)])
+    model_end = time()
+
+    mdl = sgp.solve()
+    solve_end = time()
+    print("\n{}-{}-{}\nvariables : {} clauses : {}".format(groups, size,
+          weeks, sgp.idx_next_var-1, len(sgp.cnf.clauses)), file=out)
+    print("model : {:.2f}s solver : {:.2f}s".format(
+        model_end - model_start, solve_end - model_end), file=out)
+
+    sol = sgp.model_to_set_solution(mdl)
+    if sol != []:
+        for i in range(groups*weeks):
+            if i % groups == 0:
+                print("\n|", end="", file=out)
+            for elmt in sol[i]:
+                print("{:<2}".format(elmt), end=" ", file=out)
+            print("| ", end="", file=out)
+        print("", file=out)
+    else:
+        print("UNSAT", file=out)
+    return sol
+
+
+def solve_sgp_adv(g, s, w, f=None, name=None):
+    sgp_set_to_sat(g, s, w, f, sgp=Set_Sat_Adv())
+
+
+def solve_sgp_basic(g, s, w, f=None, name=None):
+    sgp_set_to_sat(g, s, w, f, sgp=Set_Sat())
